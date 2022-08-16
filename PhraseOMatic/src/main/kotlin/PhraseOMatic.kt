591aa4ae076d0b013ec713fd9425175561973407
2Kelvin <kvin.mwangi254@gmail.com>
2022-08-16 22:14:20 +0300

    learnt about array types

--- PhraseOMatic/src/main/kotlin/PhraseOMatic.kt
+++ PhraseOMatic/src/main/kotlin/PhraseOMatic.kt
@@ -1,3 +1,7 @@
+// String templates allow you to evaluate complex expressions from inside a String
+// e.g. var result = "myArray is ${if (myArray.size > 10) "large" else "small"}"
+// you can use Kotlin’s built-in "Random" functions instead to generate random numbers e.t.c
+// e.g. kotlin.random.Random.nextInt()  -> generates a random Integer
 fun main() {
     val wordArray1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
     val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
@@ -16,11 +20,19 @@ fun main() {
 }
 
 // reusable function that calculates the size of an array
-fun calculateArraySize(array: Array<String>): Int {
-    return (array.size)
+fun calculateArraySize(anArray: Array<String>): Int {
+    return (anArray.size)
 }
 
 //function random generator
 fun randomArrayIndex(theArraySize: Int): Int {
     return (Math.random() * theArraySize).toInt()
 }
+
+// The compiler looks at the type of each item in the array, and infers what type of items the array should contain forever
+// you get a compile error if you try to add an item of a different type in the array
+// Arrays hold items of a specific type e.g. Ints, Strings, e.t.c
+// You can either let the compiler infer the type from the array’s values, or
+// ###  explicitly define the array type using "Array<Type>" ###
+// e.g ->  var byteArray: Array<Byte> = arrayOf(1,2,3)
+// simply specify the type of array you want to create by putting the type between the angle brackets (< >)
