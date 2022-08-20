40196ddcec12a5c2a1398cb7ff54ffe503439647
2Kelvin <kvin.mwangi254@gmail.com>
2022-08-20 12:19:58 +0300

    learnt about initializer blocks

--- ClassesAndObjects/src/main/kotlin/classesAndObjects.kt
+++ ClassesAndObjects/src/main/kotlin/classesAndObjects.kt
@@ -6,10 +6,14 @@ fun main() {
     simba.weight = 27
     println("Simba's new weight is ${simba.weight} kilograms")
     println("Hey ${simba.name}. You're a ${simba.breed} breed that weighs ${simba.weight} kilograms.")
+    simba.activities[0] = "running"
+    println("${simba.name} loves ${simba.activities[0]}")
+    println()
 
     // creating Pus, a Cat object
     val pus = Cat("Puss", "White", "Female")
     println("Hi, am ${pus.name} and I ${pus.purr()}")
+    println()
 
     // creating an array of dogs
     // since the dog objects in the array are of the same class; Dog, the array type is --> Array<Dog> (array of class Dog)
@@ -20,6 +24,7 @@ fun main() {
 
     // accessing $ printing Dog object's 2 properties through the array
     println("Hi, am ${dogs[1].name} and am a ${dogs[1].breed}. ${dogs[1].bark()} ${dogs[1].bark()}")
+    println()
 
     // updating a Dog object's breed property through the array & calling its method
     dogs[0].breed = "German Shepherd"
@@ -27,9 +32,21 @@ fun main() {
 }
 
 // Class Dog
-class Dog(val name: String, var breed: String, var weight: Int) {
+class Dog(val name: String, breedParam: String, var weight: Int) {
+    // this is an initializer block
+    init {
+        println("The dog's name & weight properties have been assigned values")
+    }
+    // initially, initialized in the constructor, but I wanted to convert the breed input to capitals first
+    var breed = breedParam.uppercase()
+    // default property (all dogs created through this class will have it)
+    var activities = arrayOf("walking")
     fun bark(): String {
         return if (weight > 20) "Woof!" else "Yip!"
+    }
+    // this is an initializer block
+    init {
+        println("$name (a Dog object) has been created")
     }
 }
 
@@ -64,3 +81,23 @@ class Cat(val name: String, val color: String, val sex: String) {
 // any property defined using 'var' can also be updated
 // trying to update properties defined using 'val' will just cause a compile error
 // just like other Kotlin types, objects can also be used in arrays
+
+// properties are defined in the class' constructor
+// when creating an object, these properties are assigned values and initialized inside the object
+
+// CREATING DEFAULT PROPERTIES
+// you can do this by initializing the property inside the class
+// this means that every object created through that class, will have that property by default
+// e.g. property 'activities' above in the Dog class is a default property
+
+// INITIALIZER BLOCK(S)
+// runs when an object is being created
+// --> they are executed when the object is initialized, immediately after the constructor is called
+// to use an initializer block, use the keyword "init"
+// you can use multiple initializer blocks in one class
+// Each one runs in the order in which it appears in the class body
+// use an initializer block to insert code that you want to run when an object has been initialized
+// NOTE: the code in the Constructor runs 1st followed by the code in the Class' body
+// that means, the properties in the Constructor are created first before the Class' code block runs & creates other default properties and methods
+// if an initializer block is used in-front of a default property initialization in the class code block...
+// ...the initializer block runs 1st then the default properties are created after
