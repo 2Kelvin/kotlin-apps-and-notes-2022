f5f21a7522a5556424ed1c64212145d0b29ebbc5
2Kelvin <kvin.mwangi254@gmail.com>
2022-08-21 16:41:24 +0300

    learning about superclasses and subclasses

---
+++ SuperclassAndSubclass/src/main/kotlin/SuperclassAndSubclass.kt
@@ -0,0 +1,14 @@
+
+
+
+
+// INHERITANCE: involves;
+// -> putting common code in one class (superclass)
+// -> then allowing more specific classes (subclass) to inherit this code
+// in case of any update, you only have to update the superclass & the changes will be reflected in all its subclasses.
+// subclasses inherit all the properties & functions of their superclass
+// e.g. superclass: Car, subclass: ConvertibleCar
+// subclasses can have/ add specific properties & functions of their own
+// they can also tweak some of the properties & functions they inherit from the superclass; this is called "overriding"
+// *** A superclass contains common properties and functions that are inherited by one or more subclasses ***
+// *** A subclass can include extra properties and functions, and can override the things that it inherits ***
